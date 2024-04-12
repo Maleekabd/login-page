9415e8b4919231a4c1c8a74270c258496c1270f1
Maleekabd <malikabdurrasyid86@gmail.com>
2024-04-12 20:14:17 +0700

    fix login char

--- src/components/fieldInput.jsx
+++ src/components/fieldInput.jsx
@@ -3,12 +3,18 @@ import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 const FieldInput = () => {
-  const { loginUser } = useContext(UserContext);
+  const { loginUser, form } = useContext(UserContext);
+  console.log(form.email.length);
   const navigate = useNavigate();
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("submit berhasil");
-    navigate("/dashboard");
+    if (form.email.length < 8 || form.password.length < 8) {
+      alert("minimum char harus lebih dari atau sama dengan 8 char");
+    } else {
+      alert("submit berhasil");
+      navigate("/dashboard");
+    }
   };
 
   const handleChange = (e) => {
@@ -37,7 +43,11 @@ const FieldInput = () => {
             type="password"
             placeholder="Password"
           />
-          <button className="button-submit" type="submit" onClick={handleSubmit}>
+          <button
+            className="button-submit"
+            type="submit"
+            onClick={handleSubmit}
+          >
             Login
           </button>
         </form>
